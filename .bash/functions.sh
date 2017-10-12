605b0b8bc7b3b19d995fa3dfc6c5e1b70d4302fd
eximus <fabio_r11@hotmail.com>
2017-10-12 13:59:43 +0100

    moved blur function

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -104,21 +104,6 @@ vboxsave() {
 	vboxmanage controlvm $1 savestate
 }
 
-# Blurs area behind a window on plasma
-# if argument is given it searches for that window and gives a menu to choose, otherwise it will be interactive
-blur_window() {
-	if [ -z $1 ]; then
-		xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $(xwininfo | awk '/Window\ id/ {print $4;}')
-	else
-		local i list=( `xdotool search $1 2>/dev/null` )
-		for i in $(seq 0 $((${#list[@]}-1))); do list[$i]="${list[$i]} - `xdotool getwindowname ${list[$i]}`"; done
-		select window in "${list[@]}"; do
-			xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id ${window/[^0-9]*/}
-			break
-		done
-	fi
-}
-
 # fork to the background silently and send its output to the /dev/null
 # NOTES: generic form #>/dev/null (# is 1 by default)
 #		2>&-			---->		#>&-   (close fd)
