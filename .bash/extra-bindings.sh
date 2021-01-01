71d48383cb505246c590d8ffe5e151834285615d
eximus <fabio_r11@hotmail.com>
2021-01-01 22:26:31 +0000

    moved alt+u binding to blerc and finished gafzf func

--- .bash/extra-bindings.sh
+++
@@ -1,7 +0,0 @@
-# maintain state (\200 will save the line \201 will set it again)
-# bind -x '"\200": TEMP_LINE=$READLINE_LINE; TEMP_POINT=$READLINE_POINT'
-# bind -x '"\201": READLINE_LINE=$TEMP_LINE; READLINE_POINT=$TEMP_POINT; unset TEMP_POINT; unset TEMP_LINE'
-
-# Use Esc-u to go up a dir
-bind -x '"\206": "cd .."'
-bind '"\eu":" \C-a\C-k\206\C-m\C-y\C-h"'
--- .bash/fzf-git.bash
+++ .bash/fzf-git.bash
@@ -1,5 +1,6 @@
 
 # Functions using fzf to use in git
+# Inspired by: https://github.com/wfxr/forgit and https://github.com/bigH/git-fuzzy
 
 glfzf() {
 	git rev-parse --is-inside-work-tree >/dev/null || return 1
@@ -15,23 +16,26 @@ glfzf() {
 }
 
 gafzf() {
-	# TODO not complete yet
-	GIT_FUZZY_STATUS_ADD_KEY="${GIT_FUZZY_STATUS_ADD_KEY:-Alt-S}"
-	GIT_FUZZY_STATUS_COMMIT_KEY="${GIT_FUZZY_STATUS_COMMIT_KEY:-Alt-C}"
-	GIT_FUZZY_STATUS_RESET_KEY="${GIT_FUZZY_STATUS_RESET_KEY:-Alt-R}"
-	GIT_FUZZY_STATUS_DISCARD_KEY="${GIT_FUZZY_STATUS_DISCARD_KEY:-Alt-U}"
-	GF_STATUS_HEADER="Add all Alt-A${NORMAL}     ${GREEN}stage ${BOLD}⇡${NORMAL}  ${WHITE}$GIT_FUZZY_STATUS_ADD_KEY${NORMAL}     ${RED}${BOLD}discard ✗${NORMAL}  ${WHITE}$GIT_FUZZY_STATUS_DISCARD_KEY${NORMAL}"'
-	'"${GREEN}none ☐${NORMAL}  ${WHITE}Alt-D${NORMAL}     ${GREEN}reset ${RED}${BOLD}⇣${NORMAL}  ${WHITE}$GIT_FUZZY_STATUS_RESET_KEY${NORMAL}    * ${RED}${BOLD}commit ${NORMAL}${RED}⇧${NORMAL}  ${WHITE}$GIT_FUZZY_STATUS_COMMIT_KEY${NORMAL}"'
-	'
+	local cmd reload _stage_key _commit_key _reset_key _discard_key header
+
+	_stage_key="${GIT_FUZZY_STATUS_ADD_KEY:-Alt-S}"
+	_commit_key="${GIT_FUZZY_STATUS_COMMIT_KEY:-Alt-C}"
+	_reset_key="${GIT_FUZZY_STATUS_RESET_KEY:-Alt-R}"
+	_discard_key="${GIT_FUZZY_STATUS_DISCARD_KEY:-Alt-U}"
+
+	header="Stage: $_stage_key / Unstage: $_discard_key / Commit: $_commit_key"
+
 	cmd="git -c color.status=always -c status.relativePaths=true status -su"
 	reload="reload:$cmd"
 
 	$cmd \
-		| fzf -m --header "$GF_STATUS_HEADER" \
-		--expect="$GIT_FUZZY_STATUS_EDIT_KEY,$GIT_FUZZY_STATUS_COMMIT_KEY" \
+		| fzf -m --header "$header" \
 		--reverse -0 -m --nth 2..,.. --ansi \
+		--expect="$_commit_key" \
 		--preview="git diff --color=always -- {+2..} | delta" \
-		--bind "$GIT_FUZZY_STATUS_ADD_KEY:execute(git add {+2..})+down+$reload" \
-		--bind "$GIT_FUZZY_STATUS_DISCARD_KEY:execute(git restore --staged {2..})+down+$RELOAD"
-		# --bind "$GIT_FUZZY_STATUS_RESET_KEY:execute-silent(git fuzzy helper status_reset {+2..})+down+$RELOAD" \
+		--bind "$_stage_key:execute-silent(git add {+2..})+down+$reload" \
+		--bind "Alt-A:execute-silent(git add {+2..})+down+$reload" \
+		--bind "$_discard_key:execute-silent(git restore --staged {2..})+down+$reload" \
+		| [ "$(head -n1)" = "$_commit_key" ] && git commit || return 0
+		# --bind "$_reset_key:execute-silent(git reset {2..}))+down+$reload" \
 }
