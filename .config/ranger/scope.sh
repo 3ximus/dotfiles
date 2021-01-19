d781e8efcaaab6a3be1dfcbc789882653785b836
eximus <fabio_r11@hotmail.com>
2021-01-19 17:34:01 +0000

    added scope script preview for netcdf files

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -79,6 +79,9 @@ case "$extension" in
     7z)
         # avoid password prompt by providing empty password
         try 7z -p l "$path" && { dump | trim; exit 0; } || exit 1;;
+	# NETCDF files
+	nc)
+		try ncks -mq "$path" && { dump | trim; exit 0; } || exit 1;;
     # PDF documents:
     pdf)
         try pdftotext -l 10 -nopgbrk -q "$path" - && \
