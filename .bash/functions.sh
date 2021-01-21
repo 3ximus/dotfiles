9e4c00482ada38904c9dd3884a6478f5fe80e8d4
eximus <fabio_r11@hotmail.com>
2021-01-21 00:16:00 +0000

    fix manpage coloring

--- .bash/functions.sh
+++ .bash/functions.sh
@@ -10,7 +10,7 @@ man() { # {{{2
 		LESS_TERMCAP_md=$(printf "\e[1;36m") \
 		LESS_TERMCAP_me=$(printf "\e[0m") \
 		LESS_TERMCAP_se=$(printf "\e[0m") \
-		LESS_TERMCAP_so=$(printf "\e[1;44;30m") \
+		LESS_TERMCAP_so=$(printf "\e[1;44m") \
 		LESS_TERMCAP_ue=$(printf "\e[0m") \
 		LESS_TERMCAP_us=$(printf "\e[1;35m") \
 			man "$@"
