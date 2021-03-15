a25e22175fa0c29992d165b80f2427ec689a5f12
eximus <fabio4335@gmail.com>
2021-03-15 21:18:56 +0000

    add variables specific to pluto config

---
+++ .bash/variables.sh
@@ -0,0 +1,11 @@
+# Android
+export ANDROID=$HOME/SDKs/Android
+export PATH=$ANDROID/tools:$PATH
+export PATH=$ANDROID/tools/bin:$PATH
+export PATH=$ANDROID/platform-tools:$PATH# Android SDK
+export ANDROID_SDK=$ANDROID
+export PATH=$ANDROID_SDK:$PATH
+
+export FLUTTER=$HOME/SDKs/flutter
+export PATH=$FLUTTER/bin:$PATH
+
