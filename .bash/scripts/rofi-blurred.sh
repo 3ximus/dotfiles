13efee70b95a0fbee83dd0dbcca8aecda0596f55
eximus <fabio4335@gmail.com>
2021-04-15 00:42:12 +0100

    changed rofi theme

--- .bash/scripts/rofi-blurred.sh
+++ .bash/scripts/rofi-blurred.sh
@@ -3,7 +3,7 @@
 # Wrapper to run rofi with blurred background
 
 # /usr/bin/rofi -font 'Droid Sans Mono for Powerline 15' $HOME/.local/share/icons/Papirus-Dark/ -theme $HOME/.config/rofi/gruvbox-rofi-theme.rasi $@ &
-/usr/bin/rofi -font 'TerminessTTF Nerd Font Mono 20' $HOME/.local/share/icons/Papirus-Dark/ -theme $HOME/.config/rofi/gruvbox-rofi-theme.rasi $@ &
+/usr/bin/rofi -font 'TerminessTTF Nerd Font Mono 20' $@ &
 
 c=0
 while ! xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $(xdotool search -class 'rofi') ; do
