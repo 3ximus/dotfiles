b1c5d20b6bdb14a753bcca03737f7c5dadd0c956
eximus <fabio4335@gmail.com>
2023-02-13 22:25:14 -0500

    add firefox bookmarks backup

--- .bash/scripts/pull-request-bitbucket.sh
+++ .bash/scripts/pull-request-bitbucket.sh
@@ -5,6 +5,9 @@ set -e
 BRANCH_NAME=$(git branch --show-current)
 REMOTE=$(git remote get-url origin | sed 's@git\@bitbucket.org:\(.*/.*\).git@\1@')
 
+URL="https://api.bitbucket.org/2.0/repositories/${REMOTE}/pullrequests/"
+USERNAME="fabio_almeida_vo2"
+
 read -p "API Token file [.bitbucket_api.key]: " APIKEY
 APIKEY=${APIKEY:-$(cat .bitbucket_api.key)}
 read -p "Title: " TITLE
@@ -13,12 +16,13 @@ read -p "Target [dev]: " TARGET_BRANCH
 TARGET_BRANCH=${TARGET_BRANCH:-dev}
 
 echo -e "PR: \e[32m$BRANCH_NAME\e[m > \e[1;34m$TARGET_BRANCH\e[m : $TITLE ($APIKEY)"
-read
+echo -e "Username: \e[33m${USERNAME}\e[m"
+echo "URL: ${URL}"
+read -p 'Press any key to continue... '
 
 curl -i -X POST \
-  -H "Authorization:Bearer ${APIKEY}" \
-  -H "X-Atlassian-Token:no-check" \
-  -H "Content-Type:application/json"
+  -u "${USERNAME}:${APIKEY}" \
+  -H "Content-Type: application/json" \
   -d "{
       \"title\": \"${TITLE}\",
       \"source\": {
@@ -32,4 +36,4 @@ curl -i -X POST \
         { \"account_id\": \"5d94bd8d5247770c24548801\" },
         { \"account_id\": \"5f6e109058ea7b007091eb32\" }
       ]
-    }" "https://bitbucket.org/api/2.0/repositories/${REMOTE}/pullrequests/"
+    }" $URL
