7d9e146531c90c355f42ebab7eb37c88b094139a
eximus <fabio4335@gmail.com>
2023-04-17 19:24:51 -0400

    updates to vimrc with new context.vim plugin

--- .bash/scripts/spotify-nowplaying.5s.sh
+++ .bash/scripts/spotify-nowplaying.5s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-data=$(qdbus --literal org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | sed 's/.*"xesam:artist"[^"]*"\([^"]*\)".*"xesam:title"[^"]*"\([^"]*\)".*/\2 - \1/')
-[[ -z $data ]] && echo -n '_' || echo -n "<font size=4 color='#b3deef'>♫</font> ${data} | size=16"
+data=$(qdbus --literal org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | sed 's/.*"xesam:artist"[^"]*"\([^"]*\)".*"xesam:title"[^"]*"\([^"]*\)".*/\2 - \1/' | cut -c -60)
+[[ -z $data ]] && echo -n '_' || echo -n "<font size=4 color='#b3deef'>♫</font> ${data} | size=14"
