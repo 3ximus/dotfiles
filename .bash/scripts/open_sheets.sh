90393967d7f604f647e62936535904ed9e78298a
eximus <fabio4335@gmail.com>
2023-08-21 13:07:48 -0400

    script to upload and open file in google sheets

---
+++ .bash/scripts/open_sheets.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Reads file contents from stdin
+# the first argument is the name of the file, if omited 'temp' is given
+# NOTE Depends on ./extract_cookies.sh script
+
+$(dirname ${BASH_SOURCE[0]})/extract_cookies.sh > /tmp/sheets_upload_cookies.txt
+
+# save file form
+cat > /tmp/curl_upload.xlsx << EOF
+--lygsa2ob8du8
+content-type: application/json; charset=UTF-8
+
+{"title":"$1","mimeType":"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"}
+--lygsa2ob8du8
+content-transfer-encoding: base64
+content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+
+$(cat $1 <&0 | base64 -w0)
+--lygsa2ob8du8--
+EOF
+
+FILEID=$(curl 'https://clients6.google.com/upload/drive/v2internal/files?uploadType=multipart&supportsTeamDrives=true&fields=title%2Cid&pinned=false&convert=true&openDrive=false&reason=907&syncType=0&errorRecovery=false&key=AIzaSyAw-cTyp9Xotzvu3vNDWhDU3E9NConkKxQ' \
+	-X POST \
+	--cookie "/tmp/sheets_upload_cookies.txt" \
+	-H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0' \
+	-H 'Accept-Encoding: gzip, deflate, br' \
+	-H 'authorization: SAPISIDHASH 1692633207_9ac30f5ba90100c8cab2c0e07f0c36a37663869c_u' \
+	-H 'Origin: https://docs.google.com' \
+	-H 'content-type: multipart/related; boundary="lygsa2ob8du8"' \
+	--data-binary '@/tmp/curl_upload.xlsx' \
+	| sed -n '/id/ s/.*: "\([a-zA-Z0-9\-]\+\)",.*/\1/p')
+
+# open in new tab
+firefox --new-tab "https://docs.google.com/spreadsheets/d/$FILEID"
+
+rm /tmp/sheets_upload_cookies.txt /tmp/curl_upload.xlsx
