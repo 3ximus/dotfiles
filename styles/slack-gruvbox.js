e72302eba7816d2bd5de9008e3db972686ea0e45
eximus <fabio4335@gmail.com>
2023-09-21 14:23:45 -0400

    patch the F out of slack even more

--- styles/slack-gruvbox.js
+++ styles/slack-gruvbox.js
@@ -35,6 +35,8 @@ document.addEventListener("DOMContentLoaded", function () {
       --sk_highlight: 142,192,124;
       --sk_highlight_hover: 250,189,47;
       --sk_highlight_accent: 142,192,124;
+      --dt_color-content-pry: 235,219,178;
+      --dt_color-content-sec: 235,219,178;
     }
     .p-ia4_top_nav {
       background-color: #282828 !important;
