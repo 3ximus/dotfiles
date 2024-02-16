ef28d12907ca9d4709d5b9d3b4fa7732c452488b
eximus <fabio4335@gmail.com>
2024-02-16 16:10:20 -0500

    add menubar patch script for postman

--- install/patch/postman.sh
+++ install/patch/postman.sh
@@ -7,7 +7,12 @@ if [[ ! -d styles ]] ; then
 	exit
 fi
 
-# TODO add this patch https://github.com/postmanlabs/postman-app-support/issues/11197#issuecomment-1917636907
+menuBarPatchFile="/opt/Postman/app/resources/app/services/windowManager.js"
+if [[ -f $menuBarPatchFile  ]] ; then
+	ln=$(grep -n isDarwin $menuBarPatchFile | tail -n1 | cut -d: -f1)
+	inject="if (process.platform === 'linux') { Object.assign(browserWindowOptions, { autoHideMenuBar: true });}"
+	sed -i "$((ln -2)) a $inject" $menuBarPatchFile
+fi
 
 JS=$(cat "styles/postman-gruvbox.js")
 sed -i '/EXIMUS PATCH/d' /opt/Postman/app/resources/app/preload/desktop/index.js
