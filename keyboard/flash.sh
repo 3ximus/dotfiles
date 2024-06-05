981a961583ec1e780824ce1cb3a7f472741751b4
eximus <fabio4335@gmail.com>
2024-06-05 09:53:16 -0400

    yet another keeb map change

---
+++ keyboard/flash.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Script to flash my corne
+
+if sudo dfu-programmer atmega32u4 get ; then
+	sudo dfu-programmer atmega32u4 erase
+	sudo dfu-programmer atmega32u4 flash keyboard/crkbd_rev1_layout_split_3x6_3.hex
+	sudo dfu-programmer atmega32u4 reset
+fi
