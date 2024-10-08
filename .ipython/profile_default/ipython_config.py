67dc3aae31753f236a460df96af0e47295348b0b
eximus <fabio4335@gmail.com>
2024-10-08 11:22:42 -0400

    ipython prompt fixes

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -2,6 +2,8 @@ import IPython.terminal.prompts as prompts
 from prompt_toolkit.application import get_app
 from prompt_toolkit.key_binding.vi_state import InputMode
 from pygments.token import _TokenType, Token
+from warnings import filterwarnings
+
 
 import sys, os
 sys.path.append(os.path.expanduser('~/.ipython/profile_default/'))
@@ -10,27 +12,35 @@ from gruvbox import Color, GruvboxStyle
 config = get_config()  # type: ignore # noqa: E0602
 
 class Prompt(prompts.Prompts):
-	"""Custom IPython prompt."""
-
-	_before: str = "["
-	_after: str = "] >> "
 	_continuation: str = "… "
 
 	def in_prompt_tokens(self):
-		"""Return in prompt."""
+		venv_prompt = []
 		if get_app().vi_state.input_mode == InputMode.INSERT:
 			prompt_token = prompts.Token.InsertPrompt
 			num_token = prompts.Token.InsertPromptNum
+			if 'VIRTUAL_ENV' in os.environ:
+				venv_prompt = [
+					(prompt_token, ' ('),
+					(num_token, str(os.environ.get('VIRTUAL_ENV_PROMPT'))),
+					(prompt_token, ')'),
+				]
 		else:
 			prompt_token = prompts.Token.NavPrompt
 			num_token = prompts.Token.NavPromptNum
-
-		return [(prompt_token, '>>> '),]
-		# return [
-		#	  (prompt_token, self._before),
-		#	  (num_token, str(self.shell.execution_count)),
-		#	  (prompt_token, self._after),
-		# ]
+			if 'VIRTUAL_ENV' in os.environ:
+				venv_prompt = [
+					(prompt_token, ' ('),
+					(num_token, str(os.environ.get('VIRTUAL_ENV_PROMPT'))),
+					(prompt_token, ')'),
+				]
+		return [
+			(prompt_token, '['),
+			(num_token, str(self.shell.execution_count)),
+			(prompt_token, ']'),
+			*venv_prompt,
+			(prompt_token, '>> '),
+		]
 
 	def continuation_prompt_tokens( self, width = None):
 		"""Return continuation prompt."""
@@ -38,9 +48,7 @@ class Prompt(prompts.Prompts):
 			prompt_token = prompts.Token.InsertPrompt
 		else:
 			prompt_token = prompts.Token.NavPrompt
-
-		return [(prompt_token, " " * (self._width()))]
-		# return [(prompt_token, " " * (self._width() - 2) + self._continuation)]
+		return [(prompt_token, " " * (self._width() - 2) + self._continuation)]
 
 	def out_prompt_tokens(self):
 		"""Return out prompt."""
@@ -60,3 +68,4 @@ config.TerminalInteractiveShell.highlighting_style_overrides = {
 	Token.InsertPromptNum: f"{Color.neutral_purple} bold",
 	Token.NavPromptNum: f"{Color.neutral_blue} bold",
 }
+filterwarnings("ignore", "Attempting to work in a virtualenv")
