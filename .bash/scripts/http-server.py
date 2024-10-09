a1f5e255cbaea776dd506a87a32e0c667116d845
eximus <fabio4335@gmail.com>
2024-10-09 14:06:22 -0400

    further improvements to http-server.py

--- .bash/scripts/http-server.py
+++ .bash/scripts/http-server.py
@@ -6,45 +6,48 @@
 #  - saving files in body into an output directory
 
 import http.server
+from http import HTTPStatus
 import argparse
 import sys
 import os
+import time
 import tempfile
 
 REQUEST_COUNT = 0
 
-
 class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
   def do_POST(self):
-    # store post data on the handler to be accessed later
-    self.data = self.rfile.read(int(self.headers['Content-Length']))
-
-    self.verbose_print()
-
-    if args.output:
-      self.save_file()
-
-    self.send_response(200)
-    self.end_headers()
-    self.wfile.write(b'POST Request Successfull\n')
+    try:
+      size = int(self.headers['Content-Length'])
+      # store post data on the handler to be accessed later
+      self.data = self.rfile.read(int(self.headers['Content-Length']))
+      if args.output: self.save_file()
+      self.log(HTTPStatus.OK, size)
+      self.verbose_print()
+      self.send_response(HTTPStatus.OK)
+      self.end_headers()
+      self.wfile.write(b'POST Request Successfull\n')
+    except ValueError:
+      self.log(HTTPStatus.INTERNAL_SERVER_ERROR)
+      self.send_response(HTTPStatus.INTERNAL_SERVER_ERROR)
+      self.end_headers()
+      self.wfile.write(b'POST Request Failed\n')
 
   def do_GET(self):
+    super().do_GET()
+    self.log(self.statuscode or HTTPStatus.OK)
     self.verbose_print()
 
-    # this will serve self.directory
-    super().do_GET()
-
   def verbose_print(self):
-    global REQUEST_COUNT
-    REQUEST_COUNT += 1
-    if args.verbose > 0:
-      print(f'+ \x1b[1;34mREQUEST #{REQUEST_COUNT}\x1b[m')
+    if args.headers:
       print(self.headers)
-    if args.verbose > 1 and hasattr(self, 'data'):
+    if args.body and hasattr(self, 'data'):
       try:
         print(self.data.decode())
       except UnicodeDecodeError:
         print(self.data.hex())
+    if args.headers or (args.body and hasattr(self, 'data')):
+      print('-'*10,REQUEST_COUNT)
 
   def save_file(self):
     outdir = os.path.join(args.directory, args.output)
@@ -55,16 +58,35 @@ class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
       print(f'+ \x1b[1;34msaving #{REQUEST_COUNT} > {ofile.name}\x1b[m')
       ofile.write(self.data)
 
+  def log(self, code='-', size='-'):
+    global REQUEST_COUNT
+    now = time.time()
+    year, month, day, hh, mm, ss, _, _, _ = time.localtime(now)
+    timestamp = "%02d-%02d-%04d %02d:%02d:%02d" % (day, month, year, hh, mm, ss)
+    REQUEST_COUNT += 1
+    print(f' +{REQUEST_COUNT} {timestamp} {self.address_string()}  |  {self.requestline} {code} {size}')
+
+  def log_error(self, format, *args):
+    sys.stderr.write(f'\033[1;31m -\033[m {format % args}\n')
+
+  def send_response(self, code, message=None):
+    self.statuscode = code
+    super().send_response(code, message);
+
+  def send_error(self, code, message=None, explain=None):
+    self.statuscode = code
+    super().send_error(code, message, explain)
+
+  def log_request(self, *_):
+    pass
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(prog='Simple HTTP Server wrapper',)
-  parser.add_argument('port', nargs='?', default=8000,
-                      type=int, help="(default: %(default)s)")
-  parser.add_argument('-v', '--verbose', action='count', default=0,
-                      help="print headers. If given multiple times also prints the body")
-  parser.add_argument('-d', '--directory', default=os.getcwd(), nargs='?',
-                      help="serve this directory (default: './')")
-  parser.add_argument('-o', '--output',
-                      help="save post data in given OUTPUT directory. The base directory of the output destination is determined with the -d flag ( DIRECTORY/OUTPUT ). If absolute path is given then that's used instead")
+  parser.add_argument('port', nargs='?', default=8000, type=int, help="(default: %(default)s)")
+  parser.add_argument('-H', '--headers', action='store_true', help="print headers")
+  parser.add_argument('-B', '--body', action='store_true', help="print body")
+  parser.add_argument('-d', '--directory', default=os.getcwd(), nargs='?', help="serve this directory (default: './')")
+  parser.add_argument('-o', '--output', help="save post data in given OUTPUT directory. The base directory of the output destination is determined with the -d flag ( DIRECTORY/OUTPUT ). If absolute path is given then that's used instead")
   args = parser.parse_args()
 
   class Server(http.server.ThreadingHTTPServer):
