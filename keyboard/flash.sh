ed79419f7cae435b9db651b062e2ea072281af26
eximus <fabio4335@gmail.com>
2025-01-26 23:52:43 -0500

    add 36 keyboard layout

--- keyboard/flash.sh
+++ keyboard/flash.sh
@@ -3,16 +3,19 @@
 # Script to flash my corne
 
 if [[ ! -d keyboard ]] ; then
-	echo "Run from parent directory './install/flash.sh'"
+	echo "Run from parent directory './keyboard/flash.sh'"
 	exit
 fi
 
+select firmware in ./keyboard/*.hex ; do break; done
+
 # ask for sudo password first
+sudo echo Flashing ${firmware}...
 sudo echo Sleeping for 5 seconds. Hit the reset button now!
 sleep 5
 
 if sudo dfu-programmer atmega32u4 get ; then
 	sudo dfu-programmer atmega32u4 erase
-	sudo dfu-programmer atmega32u4 flash keyboard/crkbd_rev1_layout_split_3x6_3_mine.hex
+	sudo dfu-programmer atmega32u4 flash $firmware
 	sudo dfu-programmer atmega32u4 reset
 fi
