205314ed1804d45f3ccea17f416da7e15db87d3d
eximus <fabio4335@gmail.com>
2025-05-26 13:07:36 -0400

    minor nvim tweaks

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -13,7 +13,10 @@ require("gruvbox").setup({
 
     -- Sneak
     Sneak = { link = 'Search' },
-    SneakCurrent = { link = 'IncSearch' },
+    SneakCurrent = { link = 'CurSearch' },
+    SneakScope = { link = 'CurSearch' },
+    SneakLabelMask = { link = 'LineNr' },
+    SneakLabel= { link = 'Search' },
 
     -- Markology
     MarkologyHLl = { link = 'GruvboxYellowBold' },
