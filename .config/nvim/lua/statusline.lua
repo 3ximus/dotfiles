7d28538af9c86d6607928a83f3b3af07df4de9a3
eximus <fabio4335@gmail.com>
2025-08-23 19:33:44 -0400

    tweaks.. idk

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -159,7 +159,7 @@ require("lualine").setup({
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path=1}},
     lualine_x = {'%p%% %l/%L:%c'},
     lualine_y = {},
     lualine_z = {}
